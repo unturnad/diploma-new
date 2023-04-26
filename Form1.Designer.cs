7213d38e26a9e3d1351d9ca2a5040caa18aef690
unturnad <footbolist01337@gmail.com>
2023-04-26 20:21:15 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace diploma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(218, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(657, 494);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1066, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(45, 22);
+            this.toolStripLabel1.Text = "Файл";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(64, 22);
+            this.toolStripLabel2.Text = "Розклад";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Керування";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 190);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Керування";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(12, 224);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 298);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Групи";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(935, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Group";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox1);
+            this.groupBox4.Location = new System.Drawing.Point(881, 86);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(173, 190);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Предмети";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "dsmhbfds",
+            "sdgyfuisd",
+            "sdhgsd",
+            "dsfjkdsj"});
+            this.listBox1.Location = new System.Drawing.Point(6, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(161, 164);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label2);
+            this.groupBox5.Controls.Add(this.label1);
+            this.groupBox5.Location = new System.Drawing.Point(881, 282);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(173, 98);
+            this.groupBox5.TabIndex = 3;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Занять всього: (цифра)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.label8);
+            this.groupBox6.Controls.Add(this.label7);
+            this.groupBox6.Controls.Add(this.label3);
+            this.groupBox6.Controls.Add(this.label4);
+            this.groupBox6.Location = new System.Drawing.Point(881, 282);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(173, 110);
+            this.groupBox6.TabIndex = 3;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Занять всього: (цифра)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(103, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 16);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "num2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(103, 37);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "num1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "У розкладі:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "У семестрі:";
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.label9);
+            this.groupBox7.Controls.Add(this.label5);
+            this.groupBox7.Controls.Add(this.label10);
+            this.groupBox7.Controls.Add(this.label6);
+            this.groupBox7.Location = new System.Drawing.Point(881, 398);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(173, 124);
+            this.groupBox7.TabIndex = 4;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Занять всього: (цифра)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(103, 73);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 16);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "num2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "У розкладі:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(103, 43);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 16);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "num1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "У семестрі:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(887, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(887, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 534);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox7);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Words;
+using static diploma.Form1;
+using Aspose.Words.Saving;
+using Orientation = Aspose.Words.Orientation;
+using Aspose.Words.Tables;
+
+namespace diploma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        internal static class DBHelper
+        {
+            public static SqlConnection connection;
+            public static DataSet dsLastDataSet;
+            public static SqlDataAdapter adapter;
+            public static string sLastQuery;
+            public static string Connect = @"Data Source = localhost;Initial Catalog = working_hell;Integrated Security = True";
+            public static DataTable TryQuery(string query)
+            {
+                DataTable tmp = new DataTable();
+                if (connection == null || adapter == null)
+                    TryDeffConnection();
+                else
+                {
+                    try
+                    {
+                        using (connection = new SqlConnection(Connect))
+                        {
+                            using (adapter = new SqlDataAdapter(query, connection))
+                            {
+                                dsLastDataSet = new DataSet();
+                                adapter.Fill(dsLastDataSet);
+                                sLastQuery = query;
+                                Console.WriteLine(sLastQuery);
+                            }
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Connection error:\n" + ex.Message);
+                    }
+                    if (dsLastDataSet.Tables.Count == 0) return tmp; else return dsLastDataSet.Tables[0];
+                }
+                return null;
+            }
+            public static void TryDeffConnection()
+            {
+                try
+                {
+                    using (connection = new SqlConnection(Connect))
+                    {
+                        using (adapter = new SqlDataAdapter("select * from Subject;", connection))
+                        {
+                            dsLastDataSet = new DataSet();
+                            adapter.Fill(dsLastDataSet);
+                            Console.WriteLine("default connection established");
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Conncetion error:\n" + ex.Message);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //using (WordprocessingDocument document = WordprocessingDocument.Create("D:\\diploma\\diploma.docx", WordprocessingDocumentType.Document))
+            //{
+            //    // Add a main document part
+            //    MainDocumentPart mainPart = document.AddMainDocumentPart();
+
+            //    // Create a new document
+            //    Document document12 = new Document();
+            //    mainPart.Document = document12;
+
+            //    // Create a new paragraph
+            //    Paragraph paragraph = new Paragraph(new Run(new Text("Hello, world!  BEERTODAYDIETOMORROW")));
+
+            //    // Add the paragraph to the document
+            //    document12.Body = new Body(paragraph);
+
+            //    // Save the document
+            //    mainPart.Document.Save();
+            //}
+            Document doc = new Document("D:\\diploma\\diploma.docx");
+            Section section = doc.Sections[0];
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            builder.Write("\n\n\n");
+            section.PageSetup.Orientation = Orientation.Landscape;
+            //doc.FirstSection.Body.Remove();
+            // Get the first paragraph of the first section
+            Paragraph para = doc.FirstSection.Body.FirstParagraph;
+            doc.FirstSection.Body.AppendChild(para);
+            Run newRun = new Run(doc, "Hello,123 ");
+            newRun.Font.Name = "Arial";
+            newRun.Font.Size = 12;
+            newRun.Font.Bold = true;
+
+            Run refRun = para.Runs[0];
+            // Insert text at the beginning of the paragraph
+            para.InsertBefore(newRun, refRun);
+            Table table = new Table(doc);
+            for (int row = 0; row < 6; row++)
+            {
+                // Add a new row to the table
+                Row tableRow = new Row(doc);
+                table.Rows.Add(tableRow);
+
+                for (int col = 0; col < 8; col++)
+                {
+                    // Add a new cell to the row
+                    Cell cell = new Cell(doc);
+                    tableRow.Cells.Add(cell);
+
+                    // Create a new paragraph and run, and add it to the cell
+                    Paragraph cellPara = new Paragraph(doc);
+                    //Run cellRun = new Run(doc, "Row " + (row + 1) + ", Col " + (col + 1));
+                    //cellPara.AppendChild(cellRun);
+                    cell.CellFormat.ClearFormatting();
+                    cell.CellFormat.Orientation = TextOrientation.Horizontal;
+                    cell.CellFormat.VerticalMerge = CellMerge.None;
+                    cell.CellFormat.HorizontalMerge = CellMerge.None;
+                    cell.CellFormat.WrapText = true;
+                    cell.CellFormat.FitText = true;
+                    cell.CellFormat.Borders.LineStyle = LineStyle.Single;
+                    cell.CellFormat.Shading.BackgroundPatternColor = Color.White;
+                    cell.CellFormat.TopPadding = 5.0;
+                    cell.CellFormat.BottomPadding = 5.0;
+                    cell.CellFormat.LeftPadding = 5.0;
+                    cell.CellFormat.RightPadding = 5.0;
+                    cell.AppendChild(cellPara);
+                }
+            }
+            builder.MoveToDocumentEnd();
+
+            // Add the table to the paragraph
+            //para.AppendChild(table);
+            doc.FirstSection.Body.AppendChild(table);
+            // Add the paragraph to the document
+            doc.FirstSection.Body.AppendChild(para);
+            // Save the modified document
+            doc.Save("D:\\diploma\\diploma.docx");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //using (FileStream docStream = new FileStream("D:\\diploma\\diploma.docx", FileMode.Open, FileAccess.Read))
+            //{
+            //    //Load an existing Word document.
+            //    using (WordDocument wordDocument = new WordDocument(docStream, FormatType.Automatic))
+            //    {
+            //        //Create a new instance of DocIORenderer.
+            //        using (DocIORenderer render = new DocIORenderer())
+            //        {
+            //            //Convert an entire Word document to images.
+            //            Stream[] imageStreams = wordDocument.RenderAsImages();
+            //            for (int i = 0; i < imageStreams.Length; i++)
+            //            {
+            //                //Save the stream as file.
+            //                using (FileStream fileStreamOutput = File.Create("D:\\diploma\\diploma.jpeg"))
+            //                {
+            //                    imageStreams[i].CopyTo(fileStreamOutput);
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            try
+            {
+                var doc = new Document("D:\\diploma\\diploma.docx");
+                var saveOptions = new Aspose.Words.Saving.ImageSaveOptions(Aspose.Words.SaveFormat.Png);
+                saveOptions.PageSet = new PageSet(0);
+
+                // Save the first page of the document as a PNG image
+                doc.Save("D:\\diploma\\diploma.png", saveOptions);
+            }
+            catch
+            {
+                MessageBox.Show("Закрити ворд");
+            }
+            
+
+            
+        }
+    }
+}
